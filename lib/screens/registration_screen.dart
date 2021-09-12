102446026d1271b6405dc77ccfdf92ff3436da27
Heshan <heshan.imedha@gmail.com>
2021-09-12 11:57:50 +0530

    SignOut

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -16,6 +16,7 @@ class RegistrationScreen extends StatefulWidget {
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
   final _auth = FirebaseAuth.instance;
+
   late String email;
   late String password;
 
