58c210dc3e9401c7bf3a8a28b19deb89be16bdc7
Heshan <heshan.imedha@gmail.com>
2021-09-27 07:22:17 +0530

    Changing UI with isMe

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:chit_chat/constants.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
@@ -5,7 +6,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
-import '../constants.dart';
+late String uid;
+final _auth = FirebaseAuth.instance;
+late User loggedInUser;
+final _firestore = FirebaseFirestore.instance;
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -17,15 +21,14 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-  final _firestore = FirebaseFirestore.instance;
-  final _auth = FirebaseAuth.instance;
-  late User loggedInUser;
+  final messageTextController = TextEditingController();
   late String messageText;
 
   @override
   void initState() {
     super.initState();
     getCurrentUser();
+    currentUser();
   }
 
   void getCurrentUser() async {
@@ -34,16 +37,26 @@ class _ChatScreenState extends State<ChatScreen> {
       if (user != null) {
         loggedInUser = user;
         print(loggedInUser.email);
+        final currentUser = loggedInUser.email;
       }
     } catch (e) {
       print(e);
     }
   }
 
+  currentUser() {
+    final User? user = _auth.currentUser;
+    final uid = user?.email.toString();
+    print('***UID: $uid');
+    return uid;
+  }
+
   void messagesStream() async {
     await for (var snapshot in _firestore.collection('messages').snapshots()) {
       for (var message in snapshot.docs) {
         print(message.data());
+
+        final messageSender = message.data().keys;
       }
     }
   }
@@ -79,67 +92,7 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            StreamBuilder<QuerySnapshot>(
-                stream: _firestore.collection('messages').snapshots(),
-                builder: (BuildContext context,
-                    AsyncSnapshot<QuerySnapshot> snapshot) {
-                  if (snapshot.hasError) {
-                    return const Text('Something went wrong');
-                  }
-
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    //return const Text("Loading");
-                    return const Center(
-                      child: SpinKitDoubleBounce(
-                        color: Colors.blueAccent,
-                        size: 100.0,
-                      ),
-                    );
-                  }
-                  return Expanded(
-                    child: ListView(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 20.0, horizontal: 10.0),
-                      scrollDirection: Axis.vertical,
-                      children:
-                          snapshot.data!.docs.map((DocumentSnapshot document) {
-                        Map<dynamic, dynamic> data =
-                            document.data()! as Map<String, dynamic>;
-                        return Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 10.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              Text(
-                                data['sender'],
-                                style: const TextStyle(
-                                  fontSize: 12.0,
-                                  color: Colors.black54,
-                                ),
-                              ),
-                              Card(
-                                color: Colors.blue,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(30.0),
-                                ),
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text(
-                                    data['text'],
-                                    style: const TextStyle(
-                                        fontSize: 15.0,
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.white),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        );
-                      }).toList(),
-                    ),
-                  );
-                }),
+            MessagesStream(),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -147,6 +100,7 @@ class _ChatScreenState extends State<ChatScreen> {
                 children: <Widget>[
                   Expanded(
                     child: TextField(
+                      controller: messageTextController,
                       onChanged: (value) {
                         messageText = value;
                       },
@@ -154,16 +108,17 @@ class _ChatScreenState extends State<ChatScreen> {
                     ),
                   ),
                   TextButton(
+                    child: const Text(
+                      'Send',
+                      style: kSendButtonTextStyle,
+                    ),
                     onPressed: () {
                       _firestore.collection('messages').add({
                         'text': messageText,
                         'sender': loggedInUser.email,
                       });
+                      messageTextController.clear();
                     },
-                    child: const Text(
-                      'Send',
-                      style: kSendButtonTextStyle,
-                    ),
                   ),
                 ],
               ),
@@ -172,5 +127,96 @@ class _ChatScreenState extends State<ChatScreen> {
         ),
       ),
     );
+  }
+}
+
+class MessagesStream extends StatelessWidget {
+  MessagesStream({Key? key}) : super(key: key);
+
+  late bool isMe;
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+        stream: _firestore.collection('messages').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return const Text('Something went wrong');
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            //return const Text("Loading");
+            return const Center(
+              child: SpinKitDoubleBounce(
+                color: Colors.blueAccent,
+                size: 100.0,
+              ),
+            );
+          }
+          return Expanded(
+            child: ListView(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 20.0, horizontal: 10.0),
+              scrollDirection: Axis.vertical,
+              children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                Map<dynamic, dynamic> data =
+                    document.data()! as Map<String, dynamic>;
+
+                final messageSender = data['sender'];
+                final currentUser = loggedInUser.email;
+
+                isMe = messageSender == currentUser;
+
+                //if (messageSender == currentUser) {}
+
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                  child: Column(
+                    crossAxisAlignment: isMe
+                        ? CrossAxisAlignment.start
+                        : CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        data['sender'],
+                        style: const TextStyle(
+                          fontSize: 12.0,
+                          color: Colors.black54,
+                        ),
+                      ),
+                      Card(
+                        color: isMe ? Colors.white : Colors.blue,
+                        shape: isMe
+                            ? const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.only(
+                                  topRight: Radius.circular(30.0),
+                                  bottomLeft: Radius.circular(30.0),
+                                  bottomRight: Radius.circular(30.0),
+                                ),
+                              )
+                            : const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(30.0),
+                                  bottomLeft: Radius.circular(30.0),
+                                  bottomRight: Radius.circular(30.0),
+                                ),
+                              ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            data['text'],
+                            style: TextStyle(
+                                fontSize: 15.0,
+                                fontWeight: FontWeight.bold,
+                                color: isMe ? Colors.black : Colors.white),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              }).toList(),
+            ),
+          );
+        });
   }
 }
